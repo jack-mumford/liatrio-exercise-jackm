156c09a6cc4b146a92b04d1b13f2688f9c361bbb
Jack Mumford <jack@cmumford.com>
2025-03-10 20:37:29 -0700

    Didn't have a .dockerignore file which resulted in my container have a solid 1.36gb in size... Also updated the dockerfile some more

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
+// Jack Mumford 2025
+
+// Using express
 const app = require('express')();
 const PORT = 8080;
+
+// Grab Date
 const now = new Date();
 const time = now.toLocaleTimeString();
+
+//String to be minified
 var jackMumford = `{
 "message": "My name is Jack Mumford",
 "timestamp": "${time}"
 }`;
 
+// Telling the port to listen
+app.listen(
+    PORT,
+    () => console.log(JSON.stringify(JSON.parse(jackMumford)))
+);
 
-
-//var miniResponse = JSON.stringify(JSON.parse(jackMumford));
-
-console.log(JSON.stringify(JSON.parse(jackMumford)));
-//     PORT,
-//     () => console.log(jackMumford)
-// )
+//JSON.stringify(JSON.parse(jackMumford))
+// `it's alive on http://localhost:${PORT}`
