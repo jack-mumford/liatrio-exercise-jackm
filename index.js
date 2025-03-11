f7e5d96dee070af0ab36f42ca4e1e66d810e6f02
Jack Mumford <jack@cmumford.com>
2025-03-10 18:45:20 -0700

    Finishing up express.js n stuff also removed the node module folder cause why was it there in the first place?

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const app = require('express')();
 const PORT = 8080;
+const now = new Date();
+const time = now.toLocaleTimeString();
+var jackMumford = `{
+"message": "My name is Jack Mumford",
+"timestamp": "${time}"
+}`;
 
-app.listen(
-    PORT,
-    () => console.log('its alive on http://localhost:${PORT}')
-)
+
+
+//var miniResponse = JSON.stringify(JSON.parse(jackMumford));
+
+console.log(JSON.stringify(JSON.parse(jackMumford)));
+//     PORT,
+//     () => console.log(jackMumford)
+// )
