8519a2e62ee857a8579d36c66177b12fcc35b687
Jack Mumford <jack@cmumford.com>
2025-03-19 12:26:23 -0700

    cleaned up js file

--- index.js
+++ index.js
@@ -7,18 +7,17 @@ const PORT = 80;
 
 // Information to send
 app.get(`/`, (req, res) => {
+
     // Grab Date
     const time = Date.now();
-    // have dynamic field for xtraCred
-    const githubSHA = process.env.GITHUB_SHA;
-    //String to be minified
-    // Object to be sent
+
+    //String to be minified and sent
     const jackMumford = {
         message: "My name is Jack Edward Mumford",
         timestamp: time,
-        extracredit: githubSHA,
-        extracredit1: "done"
+        extracredit: "done"
     };
+
     res.json(jackMumford); // Send JSON response // This is how I minified the string
 });
 
